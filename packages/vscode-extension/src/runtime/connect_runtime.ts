eaeb14bf0455b53d70bfdeb40fe041e05cabef1f
Krzysztof Magiera <krzys@swmansion.com>
2025-05-27 00:13:53 +0200

    Stashing work

--- packages/vscode-extension/src/runtime/connect_runtime.ts
+++ packages/vscode-extension/src/runtime/connect_runtime.ts
@@ -1,12 +1,21 @@
 import "./cdp_binding_agent";
 import inspectorBridge from "../../lib/inspector_bridge";
 import { setup } from "../../lib/network";
+import { broadcastQueryClient } from "../../lib/plugins/react-query-devtools";
+
+const devtoolPlugins = new Set();
+globalThis.__RNIDE_register_dev_plugin = (name) => {
+  devtoolPlugins.add(name);
+  inspectorBridge.sendMessage({
+    type: "devtoolPluginsChanged",
+    data: {
+      plugins: Array.from(devtoolPlugins.values()),
+    },
+  });
+};
 
 setup();
 
-inspectorBridge.sendMessage({
-  type: "devtoolPluginsChanged",
-  data: {
-    plugins: ["network"],
-  },
-});
+if (globalThis.__RNIDE_queryClient) {
+  broadcastQueryClient(globalThis.__RNIDE_queryClient);
+}
